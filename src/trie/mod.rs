e709513cd7c4b3f3bd3f6f5c87d76a09e17f638a
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2024-02-05 22:38:29 -0400

    chore: working on branch insertion

--- src/trie/mod.rs
+++ src/trie/mod.rs
@@ -1,11 +1,20 @@
 mod key;
 
-use std::ops::Index;
-
-use crate::crypto::hasher::{Blake256Hasher, Hasher};
+use crate::crypto::hasher::Hasher;
 use key::Key;
 
-use self::key::Nibble;
+fn split_child_index_from_key(
+    partial_key: &Key,
+    common_prefix_len: usize,
+) -> Result<(u8, Key), TrieError> {
+    let (child_index, rest) = match partial_key.child_index(common_prefix_len) {
+        (Some(child_index), Some(rest_partial_key)) => (child_index.to_owned(), rest_partial_key),
+        (Some(child_index), None) => (child_index.to_owned(), Key::from(vec![])),
+        _ => return Err(TrieError::CannotGetChildIndex),
+    };
+
+    return Ok((child_index, rest));
+}
 
 type StorageValue = Option<Vec<u8>>;
 
@@ -37,13 +46,7 @@ impl<H: Hasher> Leaf<H> {
     // it updates its own partial_key to be the rest after the child
     // index and return the child index
     fn as_child(&mut self, common_prefix_len: usize) -> Result<usize, TrieError> {
-        let (child_index, rest) = match self.partial_key.child_index(common_prefix_len) {
-            (Some(child_index), Some(rest_partial_key)) => {
-                (child_index.to_owned(), rest_partial_key)
-            }
-            _ => return Err(TrieError::CannotGetChildIndex),
-        };
-
+        let (child_index, rest) = split_child_index_from_key(&self.partial_key, common_prefix_len)?;
         self.partial_key = rest;
         Ok(child_index as usize)
     }
@@ -61,13 +64,7 @@ impl<H: Hasher> Leaf<H> {
 pub struct Branch<H: Hasher> {
     partial_key: Key,
     storage_value: VersionedStorageValue<H>,
-    children: [Option<Element<H>>; 16],
-}
-
-impl<H: Hasher> Branch<H> {
-    fn to_element(&self) -> Element<H> {
-        Element::Branch(Box::new(self.to_owned()))
-    }
+    children: [Node<H>; 16],
 }
 
 impl<H: Hasher> Branch<H> {
@@ -85,6 +82,16 @@ impl<H: Hasher> Branch<H> {
             storage_value: value,
             children: Default::default(),
         }
+    }
+
+    fn as_child(&mut self, common_prefix_len: usize) -> Result<usize, TrieError> {
+        let (child_index, rest) = split_child_index_from_key(&self.partial_key, common_prefix_len)?;
+        self.partial_key = rest;
+        Ok(child_index as usize)
+    }
+
+    fn to_element(&self) -> Element<H> {
+        Element::Branch(Box::new(self.to_owned()))
     }
 }
 
@@ -151,8 +158,8 @@ impl<H: Hasher> Trie<H> {
         }
 
         match element {
-            Element::Leaf(leaf) => self.insert_on_leaf(leaf, key, value), // self.insert_on_leaf(),
-            Element::Branch(branch) => unimplemented!(),
+            Element::Leaf(leaf) => self.insert_on_leaf(leaf, key, value),
+            Element::Branch(branch) => self.insert_on_branch(branch, key, value),
         }
     }
 
@@ -167,7 +174,7 @@ impl<H: Hasher> Trie<H> {
             return Ok(Some(leaf_element.to_element()));
         }
 
-        // 1. none of the keys shares a prefix key, then a new root
+        // 1. none of the keys shares a prefix key, then a new branch
         // will be created with an empty partial key, then the current leaf
         // element will be a children and the inserted key and value another children
         // 2. keys shares some prefix key
@@ -222,6 +229,66 @@ impl<H: Hasher> Trie<H> {
         let child_index = leaf_element.as_child(common_prefix_len)?;
         branch.children[child_index] = Some(leaf_element.to_element());
         Ok(Some(branch.to_element()))
+    }
+
+    fn insert_on_branch(
+        &mut self,
+        branch_element: &mut Branch<H>,
+        key: Key,
+        value: StorageValue,
+    ) -> Result<Node<H>, TrieError> {
+        if branch_element.partial_key.eq(&key) || key.0.len() == 0 {
+            branch_element.storage_value = VersionedStorageValue::RawStorageValue(value);
+            return Ok(Some(branch_element.to_element()));
+        }
+
+        // 1. none of the keys shares a prefix, a new branch will be created
+        // with empty partial key, the current branch will become a child
+        // of the new created branch and we will keep inserting the value in its
+        // respective children slot
+        // 2. keys shares some prefix key
+        //   2.a the shared prefix is equal to the current branch node, then the current
+        //       branch node keeps as it is, we find the child index of the inserted key
+        //       if the child index is None then it becomes a leaf, if it is not empty
+        //       then we call insert recursively passing the child node, key and value
+        //   2.b The shared key is equal to key being insert, then a branch node will be
+        //       created with the key and value in it, the current branch will be updated
+        //       to br child of the new created branch
+        //   2.c both shares a part of the key, then a new branch will be created containing
+        //       the shared prefix, the remaining key of the current branch will be a new node
+        //       as a child of the created branch, same for the key and value being inserted
+        //       that will be another child in the created branch
+        let common_prefix_len = branch_element.partial_key.common_length(&key);
+
+        if common_prefix_len == 0 {
+            let mut branch_empty = Branch::<H>::new_empty();
+            let child_index = branch_element.as_child(common_prefix_len)?;
+            branch_empty.children[child_index] = Some(branch_element.to_element());
+
+            let mut new_leaf = Leaf::new(key, VersionedStorageValue::RawStorageValue(value));
+            let child_index = new_leaf.as_child(common_prefix_len)?;
+
+            branch_empty.children[child_index] = Some(new_leaf.to_element());
+            return Ok(Some(branch_empty.to_element()));
+        }
+
+        if common_prefix_len == branch_element.partial_key.0.len() {
+            let (child_index, key_rest) = split_child_index_from_key(&key, common_prefix_len)?;
+            branch_element.children[child_index as usize] =
+                match branch_element.children[child_index as usize].clone() {
+                    Some(ref mut element) => self.insert_recursively(element, key_rest, value)?,
+                    None => Some(
+                        Leaf::new(key_rest, VersionedStorageValue::RawStorageValue(value))
+                            .to_element(),
+                    ),
+                };
+
+            return Ok(Some(branch_element.to_element()));
+        }
+
+        if common_prefix_len == key.0.len() {}
+
+        Err(TrieError::InsertionFailed)
     }
 }
 
@@ -468,6 +535,158 @@ mod tests {
                         ])),
                     })),
                     None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                ],
+            }))),
+        };
+
+        assert_eq!(expected, t);
+    }
+
+    #[test]
+    fn inserting_on_branches_without_common_prefix() {
+        let mut t: Trie<Blake256Hasher> = Trie::default();
+        t.insert(Key::new(&hex!("aa11")), Some(vec![0, 1, 2, 3, 4]))
+            .unwrap();
+        t.insert(Key::new(&hex!("aa22")), Some(vec![5, 0, 0, 0, 1]))
+            .unwrap();
+        t.insert(Key::new(&hex!("1133")), Some(vec![1, 1, 1, 1, 1]))
+            .unwrap();
+
+        let expected = Trie {
+            root: Some(Element::Branch(Box::new(Branch {
+                partial_key: Key::from(vec![]),
+                storage_value: VersionedStorageValue::<Blake256Hasher>::RawStorageValue(None),
+                children: [
+                    None,
+                    Some(Element::Leaf(Leaf {
+                        partial_key: Key(vec![1, 3, 3]),
+                        storage_value: VersionedStorageValue::RawStorageValue(Some(vec![
+                            1, 1, 1, 1, 1,
+                        ])),
+                    })),
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    Some(Element::Branch(Box::new(Branch {
+                        partial_key: Key(vec![10]),
+                        storage_value: VersionedStorageValue::RawStorageValue(None),
+                        children: [
+                            None,
+                            Some(Element::Leaf(Leaf {
+                                partial_key: Key(vec![1]),
+                                storage_value: VersionedStorageValue::RawStorageValue(Some(vec![
+                                    0, 1, 2, 3, 4,
+                                ])),
+                            })),
+                            Some(Element::Leaf(Leaf {
+                                partial_key: Key(vec![2]),
+                                storage_value: VersionedStorageValue::RawStorageValue(Some(vec![
+                                    5, 0, 0, 0, 1,
+                                ])),
+                            })),
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                        ],
+                    }))),
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                ],
+            }))),
+        };
+
+        assert_eq!(expected, t);
+    }
+
+    #[test]
+    fn inserting_on_branches_with_common_prefix() {
+        let mut t: Trie<Blake256Hasher> = Trie::default();
+        t.insert(Key::new(&hex!("aa11")), Some(vec![0, 1, 2, 3, 4]))
+            .unwrap();
+        t.insert(Key::new(&hex!("aa22")), Some(vec![5, 0, 0, 0, 1]))
+            .unwrap();
+        t.insert(Key::new(&hex!("aa33")), Some(vec![1, 0, 0, 0, 1]))
+            .unwrap();
+        t.insert(Key::new(&hex!("aa2211")), Some(vec![4, 4, 0, 0, 1]))
+            .unwrap();
+
+        let expected = Trie {
+            root: Some(Element::Branch(Box::new(Branch {
+                partial_key: Key(vec![10, 10]),
+                storage_value: VersionedStorageValue::RawStorageValue(None),
+                children: [
+                    None,
+                    Some(Element::Leaf(Leaf {
+                        partial_key: Key(vec![1]),
+                        storage_value: VersionedStorageValue::RawStorageValue(Some(vec![
+                            0, 1, 2, 3, 4,
+                        ])),
+                    })),
+                    Some(Element::Branch(Box::new(Branch {
+                        partial_key: Key(vec![2]),
+                        storage_value: VersionedStorageValue::RawStorageValue(Some(vec![
+                            5, 0, 0, 0, 1,
+                        ])),
+                        children: [
+                            None,
+                            Some(Element::Leaf(Leaf {
+                                partial_key: Key(vec![1]),
+                                storage_value: VersionedStorageValue::RawStorageValue(Some(vec![
+                                    4, 4, 0, 0, 1,
+                                ])),
+                            })),
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                        ],
+                    }))),
+                    Some(Element::Leaf(Leaf {
+                        partial_key: Key(vec![3]),
+                        storage_value: VersionedStorageValue::RawStorageValue(Some(vec![
+                            1, 0, 0, 0, 1,
+                        ])),
+                    })),
                     None,
                     None,
                     None,
